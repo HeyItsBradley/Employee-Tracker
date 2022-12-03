c291b7895f5ada48e7db763fcb9f2d7e59bc58bc
HeyItsBradley <bradleyrodriguez77@gmail.com>
2022-12-02 18:07:18 -0700

    fixed view all employees where title wasnt showing actual name

--- db/schema.sql
+++ db/schema.sql
@@ -16,6 +16,7 @@ CREATE TABLE role(
     department_id INT NOT NULL,
     
     FOREIGN KEY(department_id)REFERENCES department(id)
+    ON DELETE SET NULL
 );
 
 CREATE TABLE employee(
@@ -26,5 +27,6 @@ CREATE TABLE employee(
     manager_id INT,
     FOREIGN KEY(role_id)REFERENCES role(id)
     FOREIGN KEY(manager_id)REFERENCES employee(id)
+    ON DELETE SET NULL
 );
 
--- index.js
+++ index.js
@@ -35,10 +35,13 @@ const menuSelect = [
 ];
 
 function showEmployees() {
-  db.query("SELECT * FROM EMPLOYEE", (err, results) => {
-    console.table(results);
-    showMenu();
-  });
+  db.query(
+    "SELECT employee.id, employee.first_name, employee.last_name, role.title AS title, department.name AS department, role.salary, employee.manager_id AS manager FROM((employee JOIN role ON employee.role_id = role.id) JOIN department ON department.id = role.department_id) ",
+    (err, results) => {
+      console.table(results);
+      showMenu();
+    }
+  );
 }
 
 function showRoles() {
